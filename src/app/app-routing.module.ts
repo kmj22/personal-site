53908767c098cffc1eb3a24cd69597216ade85dd
Kevyn Jaremko <kmj22@njit.edu>
2019-01-08 23:10:03 -0500

    Set up basic routes Primary color scheme variables declared and implemented Employer date added Homepage job/project title style improvements Font size increased

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,11 @@ import { Routes, RouterModule } from '@angular/router';
 import {HomepageComponent} from './homepage/homepage.component';
 
 const routes: Routes = [
-  {path: '', component: HomepageComponent, pathMatch: 'full'}
+  {path: '', component: HomepageComponent, pathMatch: 'full'},
+  {path: 'resume', component: HomepageComponent},
+  {path: 'projects', component: HomepageComponent},
+  {path: 'about', component: HomepageComponent},
+  {path: 'contact', component: HomepageComponent},
 ];
 
 @NgModule({
